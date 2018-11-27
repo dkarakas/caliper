ca2026520f53ed5c67cb032929f7e4fd260b5074
Qingnan Duan <duanqn_own_1@yeah.net>
2018-11-27 11:28:49 -0500

    Fix script

--- collect.sh
+++ collect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p res
-scp $1@$2:/ssd1/$1/gopath/src/github.com/hyperledger/caliper/res/*.out ./res/*.out
+scp $1@$2:/ssd1/$1/gopath/src/github.com/hyperledger/caliper/res/*.out ./res
